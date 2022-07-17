1d647fd63c540d96d24113e506b3b14568987952
muralik011 <k.s.v.muralidhar@gmail.com>
2022-07-17 21:15:06 +0530

    update data ingestion

--- src/data_pipeline/data_ingestion.py
+++ src/data_pipeline/data_ingestion.py
@@ -1,4 +1,4 @@
-from utils.json_parser import JSONParser
+from src.utils.json_parser import JSONParser
 import psycopg2
 import pandas as pd
 import os
@@ -7,10 +7,10 @@ import os
 def connect_postgres():
     try:
         secrets_parser = JSONParser("secrets.json")
-        db_connection = psycopg2.connect(host=secrets_parser.get_values(['data_source', 'host']),
-                                         database=secrets_parser.get_values(['data_source', 'database']),
-                                         user=secrets_parser.get_values(['data_source', 'username']),
-                                         password=secrets_parser.get_values(['data_source', 'password'])
+        db_connection = psycopg2.connect(host=secrets_parser.get_values(['iris_data_source', 'host']),
+                                         database=secrets_parser.get_values(['iris_data_source', 'database']),
+                                         user=secrets_parser.get_values(['iris_data_source', 'username']),
+                                         password=secrets_parser.get_values(['iris_data_source', 'password'])
                                          )
     except Exception as conn_exception:
         print(f'UNABLE TO CONNECT TO DB SERVER\nREASON: {conn_exception}')
@@ -20,25 +20,34 @@ def connect_postgres():
         return db_connection
 
 
-def read_table_from_database(db_connection):
-    secrets_parser = JSONParser("secrets.json")
+def read_table_from_database(db_connection, table: str):
     df = None
     if db_connection is not None:
-        table = secrets_parser.get_values(['data_source', 'table'])
         cursor = db_connection.cursor()
-        cursor.execute(f"SELECT * FROM {table}")
-        df = pd.DataFrame(cursor.fetchall())
-        df.columns = [col_name[0] for col_name in cursor.description]
+        try:
+            cursor.execute(f"SELECT * FROM {table}")
+            df = pd.DataFrame(cursor.fetchall())
+            df.columns = [col_name[0] for col_name in cursor.description]
+        except psycopg2.errors.UndefinedTable as table_err:
+            print(table_err)
         cursor.close()
         db_connection.close()
     return df
 
 
 def save_raw_data():
-    df = read_table_from_database(connect_postgres())
-    config_parser = JSONParser('config.json')
-    raw_data_dir = config_parser.get_values(['data_folder', 'raw'])
-    df.to_csv(os.path.join('data', raw_data_dir, 'iris.csv'), index=False)
+    db_connection = connect_postgres()
+    secrets_parser = JSONParser("secrets.json")
+    table = secrets_parser.get_values(['iris_data_source', 'table'])
+    if (db_connection is not None) & (table is not None):
+        df = read_table_from_database(db_connection, table)
+        if df is not None:
+            config_parser = JSONParser('config.json')
+            raw_data_path = config_parser.get_values(['data_folder', 'raw'])
+            df.to_csv(raw_data_path, index=False)
+            print('Saved raw file successfully')
+        else:
+            print("couldn't save raw file")
 
 
 if __name__ == "__main__":
