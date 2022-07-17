2733164d413af95335e9dc9f3c2bf3970f02877d
muralik011 <k.s.v.muralidhar@gmail.com>
2022-07-17 18:05:15 +0530

    add db connection

--- src/data_pipeline/data_ingestion.py
+++ src/data_pipeline/data_ingestion.py
@@ -2,3 +2,19 @@ from utils.secrets_parser import SecretsParser
 import psycopg2
 
 
+
+
+def connect_postgres():
+    try:
+        secrets_parser = SecretsParser()
+        db_connection = psycopg2.connect(host=secrets_parser.get_values(['data_source', 'host']),
+                                         database=secrets_parser.get_values(['data_source', 'database']),
+                                         user=secrets_parser.get_values(['data_source', 'username']),
+                                         password=secrets_parser.get_values(['data_source', 'username'])
+                                         )
+    except Exception as conn_exception:
+        print(f'UNABLE TO CONNECT TO DB SERVER\nREASON: {conn_exception}')
+        return None
+    else:
+        print('DB SERVER CONNECTION SUCCESSFUL')
+        return db_connection
