4c6a9be440f1bee08c455da23a997beaf3ef26aa
muralik011 <k.s.v.muralidhar@gmail.com>
2022-07-17 17:27:23 +0530

    add exception handler

--- utils/secrets_parser.py
+++ utils/secrets_parser.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 
 class SecretsParser:
@@ -8,13 +7,29 @@ class SecretsParser:
 
     @staticmethod
     def read_secrets():
-        with open('secrets.json', 'r') as secrets_json:
-            secrets = json.load(secrets_json)
+        try:
+            with open('secrets.json', 'r') as secrets_json:
+                secrets = json.load(secrets_json)
+        except FileNotFoundError as fnf_err:
+            print(fnf_err)
+            secrets = None
+        except json.decoder.JSONDecodeError as json_err:
+            print(json_err)
+            secrets = None
         return secrets
 
     def get_values(self, keys: list):
         secrets = self.read_secrets()
-        value = secrets[keys[0]]
-        for key in keys[1:]:
-            value = value[key]
+        value = None
+        if secrets is not None:
+            try:
+                value = secrets[keys[0]]
+                for key in keys[1:]:
+                    value = value[key]
+            except KeyError as key_err:
+                print(key_err)
+                value = None
+            except TypeError as type_err:
+                print(type_err)
+                value = None
         return value
